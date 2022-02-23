8084477e67f298bcbb51a0cd338d77b0cd995483
OMG <stansky666@gmail.com>
2022-02-23 17:24:41 +0100

    La recuperation de donner avec vue via api

---
+++ resources/js/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import CustomerIndex from "../components/CustomerIndex.vue";
+
+const routes = [
+    {
+        path: '/dashboard',
+        name: 'customers.index',
+        component: CustomerIndex
+    }
+];
+
+export default createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+
